955fb3839f209919f92fa9b9a491db5abcddc561
Tomas Perez <tomas.p.r7@gmail.com>
2021-05-06 17:17:07 -0300

    Creacion del tp0

---
+++ TP0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	
+	char nombre[21];
+	
+	printf("Ingrese su nombre: ");
+	scanf("%s", nombre);
+	printf("Hola %s\n", nombre);
+	
+	return 0;
+}
